89ed4a57060c22f8274f4e2c16c50fbc87b931af
officialErnests <ernestspursw@gmail.com>
2025-06-03 18:56:01 +0000

    Pending changes exported from your codespace

--- Sprites.js
+++ Sprites.js
@@ -60,172 +60,172 @@ let Cards = {
         "E_Value" : null, // enemy price Def:"1"
         "W_CardDrops" : null, // how many cards are shown
     },
-    // "SSS" : {
-    //     "By" : "the noob", // crator name
-    //     "Description" : "Shoots multiple rounds", // card description
-    //     "Color" : "#a0a0a0", //cards color
-    //     "Price" : 1, // how much the card costs
-    //     "P_Size" : null, // how bigger the player gets
-    //     "P_Delay" : null, // how longer the shooting coldown is
-    //     "P_BulletAmount" : 2, // how many bullets player shoots
-    //     "P_TurnSpeed"  : 1, // how fast player turns
-    //     "P_MoveSpeed"  : 1, // how fast player moves
-    //     "P_Dmg"  : -0.2, // how much player does dmg
-    //     "P_Hp" : null, // how much player has hp
-    //     "P_BulletSpeed" : 2, // how fast is bullet
-    //     "P_BulletSize"  : -1, // how big is bullet
-    //     "P_Spread" : 0.5, // bullet spread
-    //     "P_Range"  : -100, // how far bullets trawel
-    //     "P_Money"  : null, // how much money player gets
-    //     "P_KnockBack" : 10, // bullets knockback
-    //     "P_ExpGoal" : null, // players exp goal
-    //     "E_Size" : null, // enemy size
-    //     "E_Delay" : -10, // enemy delay
-    //     "E_TurnSpeed" : null, // enemy turn speed
-    //     "E_MoveSpeed" : null, // enemy move speed
-    //     "E_Dmg" : null, // enemy dmg
-    //     "E_Hp" : null,  // enemy hp
-    //     "E_Value" : null, // enemy price
-    //     "W_CardDrops" : null, // how many cards are shown
-    // },
-    // "Juggernaut" : {
-    //     "By" : "nooxTheHumanoid",
-    //     "Description" : "Wear heavy armor",
-    //     "Color" : "#a0a0a0",
-    //     "Price" : 75,
-    //     "P_Size" : 0.5,
-    //     "P_Delay" : null,
-    //     "P_BulletAmount" : null,
-    //     "P_TurnSpeed" : null,
-    //     "P_MoveSpeed" : -0.25,
-    //     "P_Dmg" : null,
-    //     "P_Hp" : 100,
-    //     "P_BulletSpeed" : null,
-    //     "P_BulletSize" : null,
-    //     "P_Spread" : null,
-    //     "P_Range" : null,
-    //     "P_Money" : null,
-    //     "P_KnockBack" : null,
-    //     "P_ExpGoal" : null,
-    //     "E_Size" : null,
-    //     "E_Delay" : null, 
-    //     "E_TurnSpeed" : null, 
-    //     "E_MoveSpeed" : 0.25, 
-    //     "E_Dmg" : null, 
-    //     "E_Hp" : null, 
-    //     "E_Value" : null,
-    //     "W_CardDrops" : null,
-    // },
-    // "Slomo" : { 
-    //     "By" : "nooxTheHumanoid",
-    //     "Description" : "Slows down everything (except the player) in return enemies do more damage",
-    //     "Color" : "#a0a0a0",
-    //     "Price" : 1,
-    //     "P_Size" : null,
-    //     "P_Delay" : null,
-    //     "P_BulletAmount" : null,
-    //     "P_TurnSpeed" : null,
-    //     "P_MoveSpeed" : null,
-    //     "P_Dmg" : null,
-    //     "P_Hp" : null,
-    //     "P_BulletSpeed" : -2.5,
-    //     "P_BulletSize" : null,
-    //     "P_Spread" : null,
-    //     "P_Range" : null,
-    //     "P_Money" : null,
-    //     "P_KnockBack" : null,
-    //     "P_ExpGoal" : null,
-    //     "E_Size" : null,
-    //     "E_Delay" : null, 
-    //     "E_TurnSpeed" : null, 
-    //     "E_MoveSpeed" : -0.1, 
-    //     "E_Dmg" : 3, 
-    //     "E_Hp" : null, 
-    //     "E_Value" : null,
-    //     "W_CardDrops" : null,
-    // },
-    // "Whild Card" : { 
-    //     "By" : "nooxTheHumanoid",
-    //     "Description" : "drops an extra card after leveling up but enemies are slightly faster",
-    //     "Color" : "#a0a0a0",
-    //     "Price" : 1,
-    //     "P_Size" : null,
-    //     "P_Delay" : null,
-    //     "P_BulletAmount" :null,
-    //     "P_TurnSpeed" : null,
-    //     "P_MoveSpeed" : null,
-    //     "P_Dmg" : null,
-    //     "P_Hp" : null,
-    //     "P_BulletSpeed" : null,
-    //     "P_BulletSize" : null,
-    //     "P_Spread" : null,
-    //     "P_Range" : null,
-    //     "P_Money" : null,
-    //     "P_KnockBack" : null,
-    //     "P_ExpGoal" : null,
-    //     "E_Size" : null,
-    //     "E_Delay" : null, 
-    //     "E_TurnSpeed" : null, 
-    //     "E_MoveSpeed" : 0.05, 
-    //     "E_Dmg" : null, 
-    //     "E_Hp" : null, 
-    //     "E_Value" : null,
-    //     "W_CardDrops" : 1,
-    // },
-    // "Dagger" : { 
-    //     "By" : "Big K",
-    //     "Description" : "That would be your mother",
-    //     "Color" : "#a0a0a0",
-    //     "Price" : 1,
-    //     "P_Size" : null,
-    //     "P_Delay" : null,
-    //     "P_BulletAmount" : 1,
-    //     "P_TurnSpeed" : 1.3,
-    //     "P_MoveSpeed" : 1.7,
-    //     "P_Dmg" : 4,
-    //     "P_Hp" : null,
-    //     "P_BulletSpeed" : 2,
-    //     "P_BulletSize" : -1,
-    //     "P_Spread" : 0,
-    //     "P_Range" : -170,
-    //     "P_Money" : null,
-    //     "P_KnockBack" : 3,
-    //     "P_ExpGoal" : null,
-    //     "E_Size" : null,
-    //     "E_Delay" : null, 
-    //     "E_TurnSpeed" : null, 
-    //     "E_MoveSpeed" : null, 
-    //     "E_Dmg" : null, 
-    //     "E_Hp" : null, 
-    //     "E_Value" : null,
-    //     "W_CardDrops" : null,
-    // },
-    // "RATATATATA" : {
-    //     "By" : "var nelikt",
-    //     "Description" : "Exactly what the title says. Inspired by the Counter Strike Negev.",
-    //     "Color" : "#4A9EF5",
-    //     "Price" : 2500, // Nezinu kā balancot, skatoties kad nezinu kāda ekonomija būs šai spēlei
-    //     "P_Size" : 1.5,
-    //     "P_Delay" : 0,
-    //     "P_BulletAmount" : 5, // Lēni kusties ar šo    
-    //     "P_TurnSpeed" : 0.5, // Lēni kusties ar šo    
-    //     "P_MoveSpeed" : 0.25, // Lēni kusties ar šo      
-    //     "P_Dmg" : -0.1,
-    //     "P_Hp" : null,
-    //     "P_BulletSpeed" : 3, // Tam ir jābūt ātram
-    //     "P_BulletSize" : -1,
-    //     "P_Spread" : 0.4,
-    //     "P_Range" : -100,
-    //     "P_Money" : null,
-    //     "P_KnockBack" : 0.25,
-    //     "P_ExpGoal" : null,
-    //     "E_Size" : null,
-    //     "E_Delay" : null, 
-    //     "E_TurnSpeed" : null, 
-    //     "E_MoveSpeed" : null, 
-    //     "E_Dmg" : null, 
-    //     "E_Hp" : null, 
-    //     "E_Value" : null,
-    //     "W_CardDrops" : null,
-    // }
+    "SSS" : {
+        "By" : "the noob", // crator name
+        "Description" : "Shoots multiple rounds", // card description
+        "Color" : "#a0a0a0", //cards color
+        "Price" : 1, // how much the card costs
+        "P_Size" : null, // how bigger the player gets
+        "P_Delay" : null, // how longer the shooting coldown is
+        "P_BulletAmount" : 2, // how many bullets player shoots
+        "P_TurnSpeed"  : 1, // how fast player turns
+        "P_MoveSpeed"  : 1, // how fast player moves
+        "P_Dmg"  : -0.2, // how much player does dmg
+        "P_Hp" : null, // how much player has hp
+        "P_BulletSpeed" : 2, // how fast is bullet
+        "P_BulletSize"  : -1, // how big is bullet
+        "P_Spread" : 0.5, // bullet spread
+        "P_Range"  : -100, // how far bullets trawel
+        "P_Money"  : null, // how much money player gets
+        "P_KnockBack" : 10, // bullets knockback
+        "P_ExpGoal" : null, // players exp goal
+        "E_Size" : null, // enemy size
+        "E_Delay" : -10, // enemy delay
+        "E_TurnSpeed" : null, // enemy turn speed
+        "E_MoveSpeed" : null, // enemy move speed
+        "E_Dmg" : null, // enemy dmg
+        "E_Hp" : null,  // enemy hp
+        "E_Value" : null, // enemy price
+        "W_CardDrops" : null, // how many cards are shown
+    },
+    "Juggernaut" : {
+        "By" : "nooxTheHumanoid",
+        "Description" : "Wear heavy armor",
+        "Color" : "#a0a0a0",
+        "Price" : 75,
+        "P_Size" : 0.5,
+        "P_Delay" : null,
+        "P_BulletAmount" : null,
+        "P_TurnSpeed" : null,
+        "P_MoveSpeed" : -0.25,
+        "P_Dmg" : null,
+        "P_Hp" : 100,
+        "P_BulletSpeed" : null,
+        "P_BulletSize" : null,
+        "P_Spread" : null,
+        "P_Range" : null,
+        "P_Money" : null,
+        "P_KnockBack" : null,
+        "P_ExpGoal" : null,
+        "E_Size" : null,
+        "E_Delay" : null, 
+        "E_TurnSpeed" : null, 
+        "E_MoveSpeed" : 0.25, 
+        "E_Dmg" : null, 
+        "E_Hp" : null, 
+        "E_Value" : null,
+        "W_CardDrops" : null,
+    },
+    "Slomo" : { 
+        "By" : "nooxTheHumanoid",
+        "Description" : "Slows down everything (except the player) in return enemies do more damage",
+        "Color" : "#a0a0a0",
+        "Price" : 1,
+        "P_Size" : null,
+        "P_Delay" : null,
+        "P_BulletAmount" : null,
+        "P_TurnSpeed" : null,
+        "P_MoveSpeed" : null,
+        "P_Dmg" : null,
+        "P_Hp" : null,
+        "P_BulletSpeed" : -2.5,
+        "P_BulletSize" : null,
+        "P_Spread" : null,
+        "P_Range" : null,
+        "P_Money" : null,
+        "P_KnockBack" : null,
+        "P_ExpGoal" : null,
+        "E_Size" : null,
+        "E_Delay" : null, 
+        "E_TurnSpeed" : null, 
+        "E_MoveSpeed" : -0.1, 
+        "E_Dmg" : 3, 
+        "E_Hp" : null, 
+        "E_Value" : null,
+        "W_CardDrops" : null,
+    },
+    "Whild Card" : { 
+        "By" : "nooxTheHumanoid",
+        "Description" : "drops an extra card after leveling up but enemies are slightly faster",
+        "Color" : "#a0a0a0",
+        "Price" : 1,
+        "P_Size" : null,
+        "P_Delay" : null,
+        "P_BulletAmount" :null,
+        "P_TurnSpeed" : null,
+        "P_MoveSpeed" : null,
+        "P_Dmg" : null,
+        "P_Hp" : null,
+        "P_BulletSpeed" : null,
+        "P_BulletSize" : null,
+        "P_Spread" : null,
+        "P_Range" : null,
+        "P_Money" : null,
+        "P_KnockBack" : null,
+        "P_ExpGoal" : null,
+        "E_Size" : null,
+        "E_Delay" : null, 
+        "E_TurnSpeed" : null, 
+        "E_MoveSpeed" : 0.05, 
+        "E_Dmg" : null, 
+        "E_Hp" : null, 
+        "E_Value" : null,
+        "W_CardDrops" : 1,
+    },
+    "Dagger" : { 
+        "By" : "Big K",
+        "Description" : "That would be your mother",
+        "Color" : "#a0a0a0",
+        "Price" : 1,
+        "P_Size" : null,
+        "P_Delay" : null,
+        "P_BulletAmount" : 1,
+        "P_TurnSpeed" : 1.3,
+        "P_MoveSpeed" : 1.7,
+        "P_Dmg" : 4,
+        "P_Hp" : null,
+        "P_BulletSpeed" : 2,
+        "P_BulletSize" : -1,
+        "P_Spread" : 0,
+        "P_Range" : -170,
+        "P_Money" : null,
+        "P_KnockBack" : 3,
+        "P_ExpGoal" : null,
+        "E_Size" : null,
+        "E_Delay" : null, 
+        "E_TurnSpeed" : null, 
+        "E_MoveSpeed" : null, 
+        "E_Dmg" : null, 
+        "E_Hp" : null, 
+        "E_Value" : null,
+        "W_CardDrops" : null,
+    },
+    "RATATATATA" : {
+        "By" : "var nelikt",
+        "Description" : "Exactly what the title says. Inspired by the Counter Strike Negev.",
+        "Color" : "#4A9EF5",
+        "Price" : 2500, // Nezinu kā balancot, skatoties kad nezinu kāda ekonomija būs šai spēlei
+        "P_Size" : 1.5,
+        "P_Delay" : 0,
+        "P_BulletAmount" : 5, // Lēni kusties ar šo    
+        "P_TurnSpeed" : 0.5, // Lēni kusties ar šo    
+        "P_MoveSpeed" : 0.25, // Lēni kusties ar šo      
+        "P_Dmg" : -0.1,
+        "P_Hp" : null,
+        "P_BulletSpeed" : 3, // Tam ir jābūt ātram
+        "P_BulletSize" : -1,
+        "P_Spread" : 0.4,
+        "P_Range" : -100,
+        "P_Money" : null,
+        "P_KnockBack" : 0.25,
+        "P_ExpGoal" : null,
+        "E_Size" : null,
+        "E_Delay" : null, 
+        "E_TurnSpeed" : null, 
+        "E_MoveSpeed" : null, 
+        "E_Dmg" : null, 
+        "E_Hp" : null, 
+        "E_Value" : null,
+        "W_CardDrops" : null,
+    }
 };
